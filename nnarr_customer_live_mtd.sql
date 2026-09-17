15e6a2b155c4376a5cbdab9852e1be786f786b9f
dricksPD <dricks@pagerduty.com>
2026-09-17 16:19:18 -0400

    Delete nnarr_customer_live_mtd.sql

--- nnarr_customer_live_mtd.sql
+++
@@ -1,9 +0,0 @@
---Certified Metric Profile: Net New ARR
---nnARR by Customer Live MTD
-select dd.month_id as fiscal_month
-, dd.DATE_VALUE    as as_of_date
-,SUM(ARR_NET_NEW)  as ARR_NET_NEW
-from CLAUDE_MCP_DB.DATA.DW_AGG_MONTHLY_BILLING_CUSTOMER bcq
-JOIN CLAUDE_MCP_DB.DATA.DW_DATE_DIM dd ON bcq.date_id = dd.date_id
-group by 1,2
-ORDER BY fiscal_month DESC;
